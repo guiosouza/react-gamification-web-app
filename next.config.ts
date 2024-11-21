8259b3cae28e07580b2df9b4d56507aa5355c83b
unknown <guilhermedeoliveiradesouza@gmail.com>
2024-11-21 15:38:44 -0300

    pages routing configuration

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: '/pages/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
+
+
