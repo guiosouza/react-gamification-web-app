d23ffeb8cc1e18bebb07c265e7e2e631534d1def
unknown <guilhermedeoliveiradesouza@gmail.com>
2024-11-22 09:37:11 -0300

    mui added

---
+++ src/app/exp/page.tsx
@@ -0,0 +1,7 @@
+export default function Exp() {
+  return (
+    <div>
+      <h1>Exp</h1>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import TaskCard from "@/components/task-card";
+
 export default function Home() {
   return (
     <main>
       <h1>Home</h1>
+      <TaskCard />
     </main>
   );
 }
