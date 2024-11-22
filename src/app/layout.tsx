baac70d1be01c392f4be6927862b032d7a9e656d
unknown <guilhermedeoliveiradesouza@gmail.com>
2024-11-22 11:10:35 -0300

    added themes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
- 
 import "./globals.css";
+import ThemeProviders from "../providers/ThemeProviders";
+import ResponsiveAppBar from "@/components/app-bar";
 
 export const metadata: Metadata = {
   title: "Mother Base",
@@ -14,8 +15,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body >
-        {children}
+      <body>
+        <ThemeProviders>
+          <ResponsiveAppBar />
+          {children}
+        </ThemeProviders>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import TaskCard from "@/components/task-card";
-
 export default function Home() {
   return (
     <main>
       <h1>Home</h1>
-      <TaskCard />
     </main>
   );
 }
