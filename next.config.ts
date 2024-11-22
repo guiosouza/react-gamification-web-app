cd8788e56c9cde13cc4c623e5f90e4ba8b7cdc76
unknown <guilhermedeoliveiradesouza@gmail.com>
2024-11-22 09:12:02 -0300

    project restart

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  async rewrites() {
-    return [
-      {
-        source: '/:path*',
-        destination: '/pages/:path*',
-      },
-    ];
-  },
 };
 
 export default nextConfig;
-
-
