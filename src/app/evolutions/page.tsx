ed49012cf1953fef7a58e7e58c3f96818be041b7
unknown <guilhermedeoliveiradesouza@gmail.com>
2024-12-17 15:10:47 -0300

    add date filtering and exercise data fetching to Evolutions page; enhance login flow and UI components

--- src/app/evolutions/page.tsx
+++ src/app/evolutions/page.tsx
@@ -2,78 +2,70 @@
 import {
   AreaChart,
   Area,
-  XAxis,
   YAxis,
   CartesianGrid,
   Tooltip,
   ResponsiveContainer,
 } from "recharts";
 import { useEffect, useState } from "react";
-import { auth, onAuthStateChanged } from "../../lib/firebase";
+import { auth, onAuthStateChanged, database } from "../../lib/firebase";
 import { signInWithEmailAndPassword, signOut } from "firebase/auth";
-import { Button, TextField } from "@mui/material";
-
-const data = [
-  {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
+import { ref, get } from "firebase/database";
+import { Button, Card, TextField } from "@mui/material";
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import dayjs, { Dayjs } from "dayjs";
 
 function Evolutions() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [startDate, setStartDate] = useState<Dayjs | null>(
+    dayjs().startOf("month")
+  );
+  const [endDate, setEndDate] = useState<Dayjs | null>(dayjs());
+  const [exerciseData, setExerciseData] = useState<any[]>([]);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       setIsLoggedIn(!!user);
+      if (user) fetchExerciseData(user.uid); // Fetch data on login
     });
     return () => unsubscribe();
   }, []);
 
+  const fetchExerciseData = async (userId: string) => {
+    try {
+      const userRef = ref(database, `users/${userId}/data`);
+      const snapshot = await get(userRef);
+
+      if (snapshot.exists()) {
+        const rawData = snapshot.val();
+        setExerciseData(rawData);
+      } else {
+        console.log("No data available");
+        setExerciseData([]);
+      }
+    } catch (error) {
+      console.error("Error fetching data: ", error);
+    }
+  };
+
+  const filterDataByDate = (history: any[]) => {
+    if (!startDate || !endDate) return history;
+
+    const start = startDate.startOf("day").valueOf();
+    const end = endDate.endOf("day").valueOf();
+
+    return history.filter((item) => {
+      const itemDate = dayjs(item.date, "DD/MM/YYYY - HH:mm").valueOf();
+      return itemDate >= start && itemDate <= end;
+    });
+  };
+
   const handleLogin = async () => {
     try {
       await signInWithEmailAndPassword(auth, email, password);
-      setIsLoggedIn(true);
     } catch (error) {
       console.error("Error logging in", error);
       alert("E-mail or password incorrect.");
@@ -90,34 +82,49 @@ function Evolutions() {
     }
   };
 
+  const CustomTooltip = ({ active, payload }: any) => {
+    if (active && payload && payload.length) {
+      const { date, totalWeight } = payload[0].payload;
+      return (
+        <div
+          style={{
+            backgroundColor: "rgba(255, 255, 255, 0.9)",
+            color: "black",
+            border: "1px solid #ccc",
+            padding: "8px",
+            borderRadius: "5px",
+          }}
+        >
+          <p style={{ margin: 0, fontWeight: "bold" }}>{date}</p>
+          <p style={{ margin: 0 }}>{`${totalWeight} kg no total`}</p>
+        </div>
+      );
+    }
+    return null;
+  };
+
   if (!isLoggedIn) {
     return (
       <div>
         <h4>Por favor, faça login para ver os dados.</h4>
-        <div className="generic-container">
-          <TextField
-            id="outlined-email-input"
-            label="E-mail"
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            sx={{ width: "100%" }}
-          />
-        </div>
-        <div className="generic-container">
-          <TextField
-            id="outlined-password-input"
-            type="password"
-            label="Senha"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            sx={{ width: "100%" }}
-          />
-        </div>
+        <TextField
+          label="E-mail"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          sx={{ width: "100%", mb: 2 }}
+        />
+        <TextField
+          label="Senha"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          sx={{ width: "100%", mb: 2 }}
+        />
         <Button
-          sx={{ mt: 2, width: "100%" }}
           variant="contained"
           onClick={handleLogin}
+          sx={{ width: "100%" }}
         >
           Logar
         </Button>
@@ -132,49 +139,67 @@ function Evolutions() {
           Log Out
         </Button>
       </div>
-      <h4>A demo of synchronized AreaCharts</h4>
-      <ResponsiveContainer width="100%" height={200}>
-        <AreaChart
-          width={500}
-          height={200}
-          data={data}
-          syncId="anyId"
-          margin={{
-            top: 10,
-            right: 30,
-            left: 0,
-            bottom: 0,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
-        </AreaChart>
-      </ResponsiveContainer>
-      <p>Maybe some other content</p>
 
-      <ResponsiveContainer width="100%" height={200}>
-        <AreaChart
-          width={500}
-          height={200}
-          data={data}
-          syncId="anyId"
-          margin={{
-            top: 10,
-            right: 30,
-            left: 0,
-            bottom: 0,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Area type="monotone" dataKey="pv" stroke="#82ca9d" fill="#82ca9d" />
-        </AreaChart>
-      </ResponsiveContainer>
+      <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="pt-br">
+        <div className="generic-container">
+          <DatePicker
+            sx={{ width: "100%" }}
+            label="Data inicial"
+            value={startDate}
+            onChange={(date) => setStartDate(date)}
+            format="DD/MM/YYYY"
+          />
+        </div>
+        <div className="generic-container">
+          <DatePicker
+            sx={{ width: "100%" }}
+            label="Data final"
+            value={endDate}
+            onChange={(date) => setEndDate(date)}
+            format="DD/MM/YYYY"
+          />
+        </div>
+      </LocalizationProvider>
+
+      {exerciseData.map((exercise, index) => {
+        const filteredHistory = filterDataByDate(exercise.history);
+        return (
+          <Card key={index} sx={{ mb: 2, p: 2, mt: 2 }}>
+            <h4>
+              {exercise.title.charAt(0).toUpperCase() + exercise.title.slice(1)}
+            </h4>
+            {filteredHistory.length > 0 ? (
+              <ResponsiveContainer width="100%" height={200}>
+                <AreaChart
+                  data={filteredHistory
+                    .sort(
+                      (a, b) =>
+                        dayjs(a.date, "DD/MM/YYYY - HH:mm").valueOf() -
+                        dayjs(b.date, "DD/MM/YYYY - HH:mm").valueOf()
+                    )
+                    .map((item) => ({
+                      date: item.date,
+                      totalWeight: item.totalWeightLifted,
+                    }))}
+                  margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+                >
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <YAxis />
+                  <Tooltip content={<CustomTooltip />} />
+                  <Area
+                    type="monotone"
+                    dataKey="totalWeight"
+                    stroke="#8884d8"
+                    fill="#8884d8"
+                  />
+                </AreaChart>
+              </ResponsiveContainer>
+            ) : (
+              <p>Sem dados nesse período</p>
+            )}
+          </Card>
+        );
+      })}
     </div>
   );
 }
