c152c67e20bbc0d46872b96707078ab6199faa4d
unknown <guilhermedeoliveiradesouza@gmail.com>
2025-01-08 09:53:50 -0300

    Adicionar borda ao componente LevelCard

--- src/components/level-card.tsx
+++ src/components/level-card.tsx
@@ -123,7 +123,7 @@ function LevelCard({
     const levelData = getLevelData(level);
 
     return (
-      <Card sx={{ marginBottom: 2, marginTop: 2 }}>
+      <Card sx={{ marginBottom: 2, marginTop: 2, border: "2px solid #2196f3" }}>
         <CardMedia
           component="img"
           sx={{ padding: 2, borderRadius: 2 }}
