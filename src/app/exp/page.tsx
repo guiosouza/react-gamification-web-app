6f7d3ef096281322fdc287cba10cdf04a825449e
Guilherme <78989152+guiosouza@users.noreply.github.com>
2025-02-17 08:42:17 -0300

    Atualizar o page.tsx

--- src/app/exp/page.tsx
+++ src/app/exp/page.tsx
@@ -141,7 +141,7 @@ export default function Exp() {
 
     const baseMultiplierForWater = Math.floor(levelNumber / 12);
 
-    const baseMultiplierForControlMindset = Math.floor(levelNumber / 21);
+    const baseMultiplierForControlMindset = Math.floor(levelNumber / 30);
 
     switch (taskName) {
       case "Grind":
