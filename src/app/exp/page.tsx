145ebaa39fb056e05fa3a28f6edddbad7e82bb19
Guilherme <78989152+guiosouza@users.noreply.github.com>
2025-03-01 13:05:31 -0300

    Atualizar o page.tsx

--- src/app/exp/page.tsx
+++ src/app/exp/page.tsx
@@ -158,11 +158,11 @@ export default function Exp() {
         return baseMultiplierForControlMindset;
       case "Projeto": {
         const daysPassed = calculateDaysSinceStart(startDate);
-        return daysPassed * 1398;
+        return daysPassed * 1455;
       }
       case "Sono": {
         const daysPassed = calculateDaysSinceStart(startDate);
-        return daysPassed * 1112;
+        return daysPassed * 1200;
       }
       default:
         return 0;
